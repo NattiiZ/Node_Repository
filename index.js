86e1ea3e64434c39c42d867cccf1421dc8518dec
NattiiZ <nat87972@gmail.com>
2025-01-19 03:31:56 +0700

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+
+const express = require("express");
+const req = require("express/lib/request");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => 
+{
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
